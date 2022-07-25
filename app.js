873c8c5e467aa74a3248b81140ffee818aa74863
sage319 <sage319@netmarble.com>
2022-07-25 19:59:49 +0900

    getting username

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const link = document.querySelector("a");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log(username);
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function onClickLink(event) {
+    event.preventDefault();
+    console.log("clicked!");
+    console.dir(event);
 }
 
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", onClickLink);
