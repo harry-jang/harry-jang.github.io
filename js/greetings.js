4a8e6cae314da8caecf10c754166ee9270dc7388
Harry J <harry.jang@kakao.com>
2022-07-31 20:39:30 +0900

    지역/날씨 추가

--- js/greetings.js
+++ js/greetings.js
@@ -1,7 +1,6 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -26,7 +25,6 @@ function paintGreetings(username) {
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", onClickLink);
 
 const savedUserName = localStorage.getItem(USERNAME_KEY);
 
