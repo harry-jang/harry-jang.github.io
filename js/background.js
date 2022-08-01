4f9dd5c51083256599a1e48cd44c2ec2552512fd
sage319 <sage319@netmarble.com>
2022-08-01 18:52:35 +0900

    css sandbox

--- js/background.js
+++ js/background.js
@@ -10,8 +10,8 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 
-//bgImage.src = `img/${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
 
-//document.body.appendChild(bgImage);
+document.body.appendChild(bgImage);
 
-document.body.style.backgroundImage  = `url(img/${chosenImage})`;
+//document.body.style.backgroundImage  = `url(img/${chosenImage})`;
